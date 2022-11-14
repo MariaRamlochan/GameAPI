515b6b95fccf7daa11a932a8c4825e0d6e284a6e
Nafees-Syed <syednafees1997@gmail.com>
2022-11-13 23:33:10 -0500

    GameModel is done

--- game-server/includes/models/GameModel.php
+++ game-server/includes/models/GameModel.php
@@ -21,4 +21,48 @@ class GameModel extends BaseModel {
         return $data;
     }
 
+        /**
+     * Get a list of Games whose title matches or contains the provided value.       
+     * @param string $title 
+     * @return array An array containing the matches found.
+     */
+    public function getWhereLike($title) {
+        $sql = "SELECT * FROM game WHERE Title LIKE :title";
+        $data = $this->run($sql, [":title" => $title . "%"])->fetchAll();
+        return $data;
+    }
+
+    /**
+     * Retrieve an Gamne by its id.
+     * @param int $game_id the id of the game.
+     * @return array an array containing information about a given game.
+     */
+    public function getGameById($game_id) {
+        $sql = "SELECT * FROM game WHERE GameId = ?";
+        $data = $this->run($sql, [$game_id])->fetch();
+        return $data;
+    }
+
+    /**
+     * Retrieve a list of games of a given developer.
+     * @param int $game_id the id of the developer.
+     * @return array a list of games.
+     */
+    public function getGameByDeveloperId($game_id) {
+        $sql = "SELECT * FROM game WHERE DeveloperID = ?";
+        $data = $this->run($sql, [$game_id])->fetchAll();
+        return $data;
+    }
+
+    /**
+     * Retrieve a list of games of a given publisher.
+     * @param int $game_id the id of the publisher.
+     * @return array a list of games.
+     */
+    public function getGameByPublisherId($game_id) {
+        $sql = "SELECT * FROM game WHERE PublisherID = ?";
+        $data = $this->run($sql, [$game_id])->fetchAll();
+        return $data;
+    }
+
 }
